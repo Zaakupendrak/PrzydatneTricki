43c447fd6c258ae07abfbaa157efcdf2b7113e31
zqdev <szymek.jakubowski@gmail.com>
2021-05-11 13:25:08 +0200

    Update awk_get_lines.sh

--- awk_get_lines.sh
+++ awk_get_lines.sh
@@ -8,13 +8,16 @@ trace_info(){
   echo -e "\e[1;30;47m[INFO]\e[0m $1"
 }
 
+
 awk_get_lines(){
   if [ $# -lt 3 ] 
   then
     trace_error "WRONG USAGE"
     trace_info "Usage: awk_get_lines <file_name> <start_line> <end_line>"
-    exit 1
+  elif [ $2 -gt $3 ]
+  then
+    trace_error "Arg 2 value is bigger than arg 3 value"
   else
-    awk 'NR >= $2 && NR <= $3' "${1}"
-  fi;
+    awk "NR >= "$2" && NR <= "$3" {print}" "${1}"
+  fi
 }
