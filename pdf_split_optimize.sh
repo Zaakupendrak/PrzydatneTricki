5e3ec8e446c76ecfb99a47557fa7e88a77e11389
zqdev <szymek.jakubowski@gmail.com>
2022-08-21 10:53:24 +0200

    Update pdf_split_optimize.sh

--- pdf_split_optimize.sh
+++ pdf_split_optimize.sh
@@ -1,42 +1,3 @@
-pdf_extract(){
-    pdftk $1 cat $2 output "e_$1"
-}
-
-pdf_split_optimize(){
-if [[ $1 == "-h" ]] || [[ $1 == "-help" ]] || [[ $1 == "--help" ]] || [[ $# -eq 0 ]];
-then
-    echo -e "\e[36mpdf_split_optimize \e[3m\e[1mfilePath\e[0m               splits and optimize only first page of file in filePath"
-    echo -e "\e[36mpdf_split_optimize \e[3m\e[1mdirPath\e[0m                splits and optimize only first pages of files in dirPath/*.pdf files"  
-    echo -e "\e[36mpdf_split_optimize \e[3m\e[1mfilePath/dirPath -a\e[0m    flag enables splitting all pages of document; otherwise script extracts only first pages"   
-else
-    # Rozdzielenie stron na pliki
-    SP_DIR=tmp
-    mkdir -p ${SP_DIR}
-    fname="${1##*/}"
-    echo -e "\e[93m\e[1mINPUT:\e[0m   ${fname}"
-    if [ "$2" = "-a" ];
-    then
-            pdftk "$1" burst output "${SP_DIR}/%02d_${fname}"
-    else
-            pdftk "$1" cat 1 output "${SP_DIR}/${fname}"
-    fi
-    rm -f ${SP_DIR}/doc_info.txt
-
-    # Optymalizacja/kompresja
-    OP_DIR=pdf_so
-    mkdir -p ${OP_DIR}
-    for file in ${SP_DIR}/*.pdf
-    do        
-        fname="${file##*/}"
-        echo -e "\e[93m\e[1mOUTPUT:  \e[0m\e[92m${fname}\e[0m"
-        # Wyłączone verbose
-        #pdf_optimize ${file} ${OP_DIR}/${fname}
-  cp "${file}" "${OP_DIR}"/"${fname}"
-    done
-    rm -f doc_data.txt
-    rm -rf ${SP_DIR}
-fi
-}
 
 pdf_invoice(){
 if [[ $1 == "-h" ]] || [[ $1 == "-help" ]] || [[ $1 == "--help" ]] || [[ $# -eq 0 ]];
@@ -117,13 +78,13 @@ else
     then
       for file in "${SP_DIR}"/*.pdf
       do
+	    echo "kroping: $file"
         sudo krop "$file" -o "${SP_DIR}/paliwo_.${lastMonthYear}.pdf"
-        wmctrl -r 'krop' -b toggle,fullscreen
-        rm "$file" 2>/dev/null
+        rm $file
       done
     fi
 
-
+    #output dir
     OP_DIR=pdf_so
     mkdir -p ${OP_DIR}    
     for file in ${SP_DIR}/*.pdf
@@ -146,27 +107,4 @@ fi
 
 pdf_optimize(){
     gs -q -dNOPAUSE -dBATCH -dSAFER -dPDFA=2 -dPDFACompatibilityPolicy=1 -dSimulateOverprint=true -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dPDFSETTINGS=/screen -dEmbedAllFonts=true -dSubsetFonts=true -dAutoRotatePages=/None -dColorImageDownsampleType=/Subsample -dColorImageResolution=200 -dGrayImageResolution=200 -dColorImageDownsampleType=/Bicubic -dMonoImageResolution=200 -sOutputFile=${2} ${1}
-}
-
-
-trace_error(){
-  echo -e "\e[1;41m[ERROR]\e[0m $1"
-}
-
-
-trace_info(){
-  echo -e "\e[1;30;47m[INFO]\e[0m $1"
-}
-
-awk_get_lines(){
-  if [ $# -lt 3 ] 
-  then
-    trace_error "WRONG USAGE"
-    trace_info "Usage: awk_get_lines <file_name> <start_line> <end_line>"
-  elif [ $2 -gt $3 ]
-  then
-    trace_error "Arg 2 value is bigger than arg 3 value"
-  else
-    awk "NR >= "$2" && NR <= "$3" {print}" "${1}"
-  fi
 }
